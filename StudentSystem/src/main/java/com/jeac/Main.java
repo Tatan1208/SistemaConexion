a15a12b89c0c354d15746a7b3f7229e11615a070
Jonathan Avendaño <jonathanestevenamail.com>
2023-09-22 14:44:24 -0500

    Agregando conexion a base de datos de java a sql

---
+++ StudentSystem/src/main/java/com/jeac/Main.java
@@ -0,0 +1,7 @@
+package com.jeac;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ StudentSystem/src/main/java/com/jeac/connection/connectionDB.java
@@ -0,0 +1,42 @@
+package com.jeac.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLDataException;
+import java.sql.SQLException;
+
+public class connectionDB {
+    public static Connection getConnectionBD(){
+        //Objeto para guardar la conexion
+        Connection connection = null;
+
+        //variables de conexion
+        String db = "sena_db";
+        String url = "jdbc:mysql://localhost:3307/" +db;
+        String name = "root";
+        String pass = "root";
+
+        try{
+            //Sirve para comprobar el driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection(url, name, pass);
+            //Intento conectarme a la base de datos
+        }catch (ClassNotFoundException | SQLException ex){
+
+            System.out.println("Ocurrio un error en la conexion: " + ex.getMessage());
+        }
+
+        return connection;
+    }
+
+    public static void main(String[] args) {
+        Connection connection2 = getConnectionBD();
+
+        if (connection2 != null){
+            System.out.println("Todo fino pa' " + connection2 );
+        }else{
+            System.out.println("Pailas " + connection2);
+        }
+
+    }
+}
